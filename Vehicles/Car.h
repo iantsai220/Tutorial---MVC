d5d10c974ffb7685c2e2cdc2efe7f391fcce32b2
iantsai <iantsai86@gmail.com>
2015-05-09 10:58:41 -0700

    first commit

---
+++ Vehicles/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Ian Tsai on 2015-05-09.
+//  Copyright (c) 2015 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@end
---
+++ Vehicles/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Vehicles
+//
+//  Created by Ian Tsai on 2015-05-09.
+//  Copyright (c) 2015 Designated Nerd Software. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
